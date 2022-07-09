621bce0612133812b2a85f3ef0ad5a9a094e3e12
Vika Putri Ariyanti <72329350+vikaputri@users.noreply.github.com>
2022-07-09 20:08:20 +0700

    Update settings.py

--- pariwisata/settings.py
+++ pariwisata/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2@d)mz*j77p(t+3!b%sba!j-@3dec!=isyql#htp-6%k(lx53'
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
